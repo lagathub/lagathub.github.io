1063fbccf7419a4647b274af084097a70dd78b62
Herbert Kipkoech <105177885+lagathub@users.noreply.github.com>
2025-11-29 12:51:54 +0300

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -131,12 +131,6 @@ const CONFIG = {
         '',
     },
   ],
-  /** Display articles from your medium or dev account. (Optional)
-  blog: {
-    source: '', // medium | dev
-    username: '', // to hide blog section, keep it empty
-    limit: 2, // How many articles to display. Max is 10.
-  }, **/
   
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
